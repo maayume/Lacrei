893e0493e8d0fb84b4b8f7017cc59f926918d131
Amir Moufarrege <amir0883@yahoo.com.br>
2022-07-05 23:36:22 -0300

    inciando fomulario login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,49 @@
+import React from 'react';
+
+import { Container } from 'react-boostrap';
+
 export default function Login(){
     return(
-        <h1>
-            Login
-        </h1>
+        <>
+        <Container>
+            <Row>
+                <Col> 
+
+                    <h1> Criar Conta </h1>
+
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicNome">
+                            <Form.Label>Nome completo</Form.Label>
+                            <Form.Control type="text" placeholder="Digite seu nome" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Digite seu email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicSenha">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Digite sua senha" />
+                        </Form.Group>
+  
+                        <Button variant="primary" type="submit">
+                            Criar
+                        </Button>  
+
+                        <P>Quero ser um profissional!</P>
+                    </Form>
+
+                </Col>
+                <Col>
+                        <H2> Já possui conta? </H2>
+                        <p>Entre com seu Lacrei ID e aproveite os nossos profissionais da saúde.</p>
+                        <button className="btn btn-primary" type="submit">Entrar</button>
+
+
+                </Col>
+            </Row>
+        </Container>
+    </>
     );
 }
