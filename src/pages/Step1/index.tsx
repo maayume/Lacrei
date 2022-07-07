97d0ac7c004406c6cb52129a19619b031dd8663e
Mayume <mayumecristine@icloud.com>
2022-07-07 09:10:06 -0300

    Página de cadastro step 1

--- src/pages/Step1/index.tsx
+++ src/pages/Step1/index.tsx
@@ -1,48 +1,49 @@
-import { ReactNode } from "react";
-// import  Theme  from "../../components/Theme";
 import * as C from "./styles";
 
 export default function Step1(){
     return(
-        //  <Theme>
-            <C.Container>
-                <h3>Dados Profissionais</h3>
-                <h5>*Campos de preenchimento obrigatório</h5>
-                <form action="" id="form-step1" method="post">
-                    <label htmlFor="area-atuacao">
-                    <p>Área de atuação*</p>
-                    <select name="atuacao" id="atuacao" form="form-step1" placeholder="Selecione a área de atuação">
-                        <option value="Selecione a area de atuação">Selecione a área de atuação</option>
-                        <option value="Enfermagem">Enfermagem</option>
-                        <option value="Fisioterapia">Fisioterapia</option>
-                        <option value="Fonoaudiologia">Fonoaudiologia</option>
-                        <option value="Medicina">Medicina</option>
-                        <option value="Nutrição">Nutrição</option>
-                        <option value="Odontologia">Odontologia</option>
-                        <option value="Psicologia">Psicologia</option>
-                    </select>
-                    </label>
-                    <label htmlFor="conselhoProfissional">
-                    <p>Prefixo do conselho profissional*</p>
-                    <select name="conselhoProfissional" id="conselhoProfissional" form="form-step1" placeholder="conselho profissional">
-                        <option value="Prefixo de conselho profissional">Prefixo de conselho profissional</option>
-                        <option value="COREN">COREN</option>
-                        <option value="COFFITO">COFFITO</option>
-                        <option value="CREFONO ">CREFONO</option>
-                        <option value="CRM">CRM</option>
-                        <option value="CRN">CRN</option>
-                        <option value="CRO">CRO</option>
-                        <option value="CRP">CRP</option>
-                    </select>
-                    </label>
-                    <label htmlFor="conselhoProfissional">
-                    <p>Email</p>
-                    <input type="email" name="email" id="email" className="input-field" placeholder="Digite seu email"/>
-                    </label>
-                    <button id="voltar" >Voltarr</button>
-                    <button id="Avancar" >Avançar</button>
-        </form>
-            </C.Container>
-        //  </Theme>
+        <C.Container className="container">
+            <h1>Cadastro de Profissional</h1>
+            <h3>Dados Profissionais</h3>
+            <h5>*Campos de preenchimento obrigatório</h5>
+            <form action="" id="form-step1" method="post">
+                <label htmlFor="area-atuacao">
+                <p className="legenda" id="legenda-atuacao">Área de atuação*</p>
+                <select className="campo" name="atuacao" id="atuacao" form="form-step1" placeholder="Selecione a área de atuação">
+                    <option value="Selecione a area de atuação">Selecione a área de atuação</option>
+                    <option value="Enfermagem">Enfermagem</option>
+                    <option value="Fisioterapia">Fisioterapia</option>
+                    <option value="Fonoaudiologia">Fonoaudiologia</option>
+                    <option value="Medicina">Medicina</option>
+                    <option value="Nutrição">Nutrição</option>
+                    <option value="Odontologia">Odontologia</option>
+                    <option value="Psicologia">Psicologia</option>
+                </select>
+                </label>
+                <label htmlFor="conselhoProfissional">
+                <p className="legenda" id="legenda-conselho">Prefixo do conselho profissional*</p>
+                <select className="campo" name="conselhoProfissional" id="conselhoProfissional" form="form-step1" placeholder="conselho profissional">
+                    <option value="Prefixo de conselho profissional">Prefixo de conselho profissional</option>
+                    <option value="COREN">COREN</option>
+                    <option value="COFFITO">COFFITO</option>
+                    <option value="CREFONO ">CREFONO</option>
+                    <option value="CRM">CRM</option>
+                    <option value="CRN">CRN</option>
+                    <option value="CRO">CRO</option>
+                    <option value="CRP">CRP</option>
+                </select>
+                </label>
+                <label htmlFor="conselhoProfissional">
+                <p className="legenda" id="legenda-registro">Número de registro profissional*</p>
+                <input className="campo" type="text" name="registroProfissional" id="registroProfissional" placeholder="00/000000"/>
+                </label>
+            </form>
+            <h5>Carta aberta*</h5>
+            <h6>Descreva porquê deseja atuar no atendimento a comunidade LGBTQIAPN+. Quais suas motivações? De que forma você se <br></br>identifica com a causa?</h6>
+            <C.Area>
+                <button id="voltar" className="botao">Voltar</button>
+                <button id="Avancar" className="botao">Avançar</button>
+            </C.Area>
+        </C.Container>
     );
 }
--- src/pages/Step1/styles.ts
+++ src/pages/Step1/styles.ts
@@ -1,34 +1,110 @@
 import styled from "styled-components";
-import bgimg4 from "../../assets/background/bg-img-4.png"
-import bgimg3 from "../../assets/background/bg-img-3.png"
+import bgimg7 from "../../assets/background/bg-img-7.png"
 
 export const Container = styled.div`
 body{
     font-family: sans-serif;
     padding: 5%;
-    background-image: url(${bgimg3}), url(${bgimg4});
-    background-position: right bottom, left;
+    background-image: url(${bgimg7});
+    background-position: right;
     background-repeat: no-repeat;
     background-size: auto;
     min-height: 100vh;
+    font-size: 16px;
 } 
+    padding: 94px 102px 40px;
+h1{
     color: #018762;
+    font-size: 40px;
+    padding-bottom: 40px;
+}
+h3{
+    color: #018762;
+    font-size: 24px;
+    Weight: 700;
+    line-height: 36px;
+}
+h5{
+    font-size: 16px;
+    text-align: left;
+    font-weight: bold;
+}
+form{
+    display: flex;
+    font-size: 16px;
+    font-weight: bold;
+}
+.campo{
+    border: 1px solid #515151;
+    border-radius: 8px;
+    padding: 12px;
+    width: 100%;
+}
+label{
+    padding-right: 91px;
+    padding-top: 54px;
+    padding-bottom: 59px;
+}
+.legenda{
+    width:300px;
+    height:13px;
+    padding-bottom: 9px;
+}
+button{
+    padding: 10.5px 32px;
+    width: 120px;
+    height: 48px;
+    margin-top: 80px;
+    margin-left: 15%;
+    font-weight: 700;
+    font-size: 18px;
+    border-radius: 8px;
+    border-style: solid;
+    box-shadow: 0 1px 3px 0 black;
+    background-color: #018762;
+    color:#fff;
+    text-align: center;
+}
+#voltar{
+    background-color: #FFF;
+    color:#018762;
+    box-shadow: 0 1px 3px 0 black;
+    border-width: 2px;
+    border-color: #018762;
+}
+#avancar{
+    border: none;
+}
+@media only screen and (max-width: 768px) {
+    .campo {
+        width: 90%;
+    }
+
+    #legenda {
+        width: 95%;
+    }
+
+    h1,h3,h5 {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .botao {
+        width: 90%;
+        margin: 80px auto 0;
+
+    }
+    .Container{
+        display: flex;
+        flex-direction: column;
+    }
+}
+
 `
 export const Area = styled.div`
+    padding: 153px 140px;
     margin: auto;
     max-width: 980px;
     min-height:100vh;
-    display: flex;
     flex-direction: column;
-`
-export const Steps = styled.div`
-    flex: 1;
-`
-export const Sidebar = styled.div`
-    widht: 250px;
-    border-right: 1px solid #018762;
-`
-export const Page = styled.div`
-    flex: 1;
-    padding: 40px 40px;
 `
